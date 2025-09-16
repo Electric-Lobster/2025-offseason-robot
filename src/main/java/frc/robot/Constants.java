8f37391ea6f7aa888f4002beecd2451b1cf94b3c
Monsterturtle97 <monsterturtle97@gmail.com>
2025-09-16 19:58:04 -0400

    Added
    Drive train class and some constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,28 @@
 
 package frc.robot;
 
+import frc.robot.SubSystems.DriveTrain;
+
 /** Add your docs here. */
 public final class Constants {
-    
+
+public final class DriveTrain{}
+
+public static final int firstMotorId = 1; 
+
+public static final int secondMotorId = 2;
+
+public static final int thirdMotorId = 3;
+
+public static final int fourthMotorId = 4;
+
+public static int stallCurrentLimit = 55;
+
+public static int freecurrentlimit = 44;
+
+public static int 
+
 }
+
+
+
