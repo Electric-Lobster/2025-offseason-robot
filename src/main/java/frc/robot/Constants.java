0f7410a0d6ce994658c3346b19f450d2c7f31b68
Monsterturtle97 <monsterturtle97@gmail.com>
2025-09-16 23:11:56 -0400

    Fixed mistakes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,15 +6,17 @@ package frc.robot;
 
 import frc.robot.SubSystems.DriveTrain;
 
+
 /** Add your docs here. */
 public final class Constants  {
 
+   
     //Drive train constant 
 public static class DrivetrainConstants{
     
 
     //Motor Id's
-    /**Front Left Motor Id value is set to 2 
+    /**
      *leftPidControllerId
      */
     public static final int FLMotorId = 2; 
@@ -22,25 +24,32 @@ public static class DrivetrainConstants{
     public static final int BLMotorId = 3;
     public static final int BRMotorId = 6;
     
+    
     //Currents limits 
     /** The maxium current our motor can take with our permission  
-     * = 55
+     * 
      */
     public static final int stallCurrentLimit = 55;
+    
+    
     /** The amount our motor can take without blowing up 
-     * = 40 
+     * 
      */
     public static final int freeCurrentLimit = 40;
     
+
     //PIDControllId's
     /** store the turning left parameters 
-     * 0.1,0,0
+     * 
      */
     public static final double[] leftPidControllerId = {0.1,0,0};
+    
+    
     /** store the tuning right parameters
-     * 0.1,0,0
+     * 
      */
     public static final double[] rightPidControllerId = {0.1,0,0};
+    
     
     //Distance per a pulse for the encoder 
     /** Gives the distance per a pulse for the encoder 
