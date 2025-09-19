f607e15e0df424b7764c3bf05a59e79a94008e6f
Monsterturtle97 <monsterturtle97@gmail.com>
2025-09-18 22:45:35 -0400

    fixes not done, get help for it tommorow at school

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.SubSystems.DriveTrain;
 
+
+private final Joystick m_leftStick = new Joystick(0);
+  
+private final Joystick m_rightStick = new Joystick(1);
+
+private final Joystick m_operatorStick = new Joystick(2);
+
+
+
+
 public class RobotContainer {
   //Subsystems
   DriveTrain m_DriveTrain = new DriveTrain();
@@ -23,6 +33,7 @@ public class RobotContainer {
   AutoChooser autoChooser = new AutoChooser();
 
 
+  
   public RobotContainer() {
 
     configureBindings();
@@ -31,12 +42,6 @@ public class RobotContainer {
 
     
     }
-
-  private final Joystick m_leftStick = new Joystick(0);
-  
-  private final Joystick m_rightStick = new Joystick(1);
-
-  private final Joystick m_operatorStick = new Joystick(2);
 
   private void configureBindings() {}
 
