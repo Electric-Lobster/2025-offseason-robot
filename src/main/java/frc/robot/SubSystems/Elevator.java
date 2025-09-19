e7da96dc376bf09d5f36119a2161f01accdcbde8
Monsterturtle97 <monsterturtle97@gmail.com>
2025-09-19 15:24:42 -0400

    added motor variables

--- src/main/java/frc/robot/SubSystems/Elevator.java
+++ src/main/java/frc/robot/SubSystems/Elevator.java
@@ -4,9 +4,20 @@
 
 package frc.robot.SubSystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
+  
+  //motors 
+  SparkMax stage1LeftM = new SparkMax(4, MotorType.kBrushless);
+  SparkMax stage1RightM = new SparkMax(5, MotorType.kBrushless);
+  SparkMax stage2M = new SparkMax(14, MotorType.kBrushless);
+
+
+
   /** Creates a new Elevator. */
   public Elevator() {}
 
